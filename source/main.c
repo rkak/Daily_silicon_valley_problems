befce1e7198f6a4b48a6057a3f4c0fdd89e15141
HomeWork <micawal@hanmail.net>
2018-09-10 16:23:56 +0900

    day 1

---
+++ source/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "queue.h"
+#include "stack.h"
+#include "pibonacci.h"
+
+
+int main() {
+	// --------------------- pibonacci side ---------------- 
+
+	/*
+	_pibonacci_t * pibo;
+	pibo = pibonacci_initialization();
+
+	insert_pibonacci(pibo);
+	sum_pibonacci(pibo);
+	print_pibonacci(pibo);
+	print_sum_pibonacci(pibo);
+	*/
+
+}
